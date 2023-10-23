f9fcd31806eacbb17c5dacc1328b48f7fbf430eb
MedhaJoshi37 <148582329+MedhaJoshi37@users.noreply.github.com>
2023-10-23 07:48:43 +0530

    Update terra_provider.tf

--- terra_provider.tf
+++ terra_provider.tf
@@ -24,20 +24,20 @@ resource "azurerm_virtual_network" "example" {
 }
 
 module "tier1" {
-  source = "https://github.com/MedhaJoshi37/kpmg/blob/main/tier1.tf"
+  source = "git::https://github.com/MedhaJoshi37/kpmg//home/medha/kpmg/modules/tier1"
   resource_group_name = azurerm_resource_group.example.name
   location = azurerm_resource_group.example.location
 
 }
 module "tier2" {
-  source = "https://github.com/MedhaJoshi37/kpmg/blob/main/tier2.tf"
+  source = "git::https://github.com/MedhaJoshi37/kpmg//home/medha/kpmg/modules/tier2"
   resource_group_name = azurerm_resource_group.example.name
   location = azurerm_resource_group.example.location
 
 }
 
 module "tier3" {
-  source = "https://github.com/MedhaJoshi37/kpmg/blob/main/tier3.tf"
+  source = "git::https://github.com/MedhaJoshi37/kpmg//home/medha/kpmg/modules/tier3"
   resource_group_name = azurerm_resource_group.example.name
   location = azurerm_resource_group.example.location
 }
